9cdd8e0f912aedbe1a3ae3c40d215218b7450dfc
CWarner <world7one@gmail.com>
2024-03-20 14:37:51 -0700

    create symlinks to store dir

--- setup.sh
+++ setup.sh
@@ -6,6 +6,15 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 
+# Create symlink to store directory
+storage_dir="../store" # on my laptop
+storage_dir="~/Data/ddpm" # on CatScan
+if [ ! -d "store" ]; then
+	echo "creating symlink from store to $storage_dir"
+	ln -s $storage_dir store
+fi
+
+
 # Convert .ipynb to .py to run them from commandline
 conv_files=("sample" "manipulate" "interpolate" "autoencoding")
 for element in "${conv_files[@]}"; do
